4a3399d02d8eb86f474d54724bb8aedafc5006ef
hannahilea <hannahilea@users.noreply.github.com>
2024-10-29 22:31:47 -0400

    current working version - playable

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 """
-This example acts as a BLE HID keyboard to peer devices.
-Attach one button with a pullup resistor and one potentiometer to Feather nRF52840
-  the button will send a configurable keycode to mobile device or computer
-  the slider will send a mouse movement in the y-axis direction to mobile device or computer
+Controller for Trombone Champ by acting as a BLE HID keyboard to peer devices.
+  Attach one button with a pullup resistor and one potentiometer to Feather nRF52840
+  The button will send a configurable keycode to mobile device or computer;
+  the slider will send a mouse movement in the y-axis direction to mobile device or computer.
 """
 import board
 import math
@@ -19,12 +20,18 @@ from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.mouse import Mouse
 
-button_1 = DigitalInOut(board.A2)  # D11)
+# Global constants
+MAX_SLIDER_Y_VALUE = 900 # roughly corresponds to pixel height of screen
+CHANGE_TOLERANCE = 12    # "pixel" change threshold; above this tolerance triggers mouse movement
+NUM_SMOOTHED_VALUES = 60 # Buffer for running average of slider values
+
+# Initialize the onboard controls
+button_1 = DigitalInOut(board.A2)
 button_1.direction = Direction.INPUT
 pot_in = AnalogIn(board.A1)
 
+# Set up bluetooth connection
 hid = HIDService()
-
 device_info = DeviceInfoService(
     software_revision=adafruit_ble.__version__, manufacturer="Adafruit Industries"
 )
@@ -34,7 +41,7 @@ scan_response = Advertisement()
 scan_response.complete_name = "CircuitPython HID"
 
 ble = adafruit_ble.BLERadio()
-ble.name = "Hannex Feather Trombone Champ"
+ble.name = "Hannex Trombone Champ"
 if not ble.connected:
     print("advertising")
     ble.start_advertising(advertisement, scan_response)
@@ -42,58 +49,61 @@ else:
     print("already connected")
     print(ble.connections)
 
+# Set up keyboard
 kbd = Keyboard(hid.devices)
 kl = KeyboardLayoutUS(kbd)
 mouse = Mouse(hid.devices)
 
-
+# Helper functions for slider mapping
 def get_voltage(pin):
     return (pin.value * 3.3) / 65536
-
 
 def map_pot_percent(voltage, v_min, v_max):
     f = (voltage - v_min) / (v_max - v_min)
     return math.trunc(f * 100)
 
+def main_event_loop():
+    while True:
+        while not ble.connected:
+            pass
+        print("Bluetooth connected!")
 
-MAX_SLIDER_Y_VALUE = 700
-CHANGE_TOLERANCE = 10
-NUM_SMOOTHED_VALUES = 60
+        button_state = False
+        slider_buffer = [0 for _ in range(NUM_SMOOTHED_VALUES)]
+        slider_previous = 0.0
+        x = get_voltage(pot_in)
+        while ble.connected:
+            # Get slider values
+            voltage = get_voltage(pot_in)
+            slider_current = MAX_SLIDER_Y_VALUE * map_pot_percent(voltage, 1.62, 2.13) / 100
+            slider_buffer.append(slider_current)
+            slider_buffer.pop(0)
 
-while True:
-    while not ble.connected:
-        pass
-    print("Start typing:")
+            slider_current = sum(slider_buffer) / NUM_SMOOTHED_VALUES
+            diff = math.trunc(slider_previous - slider_current)
 
-    button_state = False
-    slider_value = 0.0
-    slider_values = [0 for _ in range(NUM_SMOOTHED_VALUES)]
-    while ble.connected:
-        # check/update button state
-        new_value = button_1.value
-        if button_state != new_value:
-            button_state = new_value
-            print("Value changed!")  # debug
-            if button_state:
-                print("Down")
-                # kbd.press(Keycode.E) # TODO-future: use different key value for click?
-            else:
-                # kbd.release(Keycode.E)
-                mouse.move(y=100)
+            # check/update button state
+            new_value = button_1.value
+            if button_state != new_value:
+                button_state = new_value
+                print("Value changed!")  # debug
+                if button_state:
+                    kbd.press(Keycode.SPACE)
+                    print(x, voltage, x - voltage)
+                    pass
+                else:
+                    kbd.release(Keycode.SPACE)
+                    pass
 
-        # check/update mouse state
-        voltage = get_voltage(pot_in)
-        slider_current = MAX_SLIDER_Y_VALUE * map_pot_percent(voltage, 1.62, 2.13) / 100
-        slider_values.append(slider_current)
-        slider_values.pop(0)
+            # Use slider voltage to update mouse position
+            elif abs(diff) >= CHANGE_TOLERANCE:
+                #print(slider_current, voltage, diff)
+                mouse.move(y=diff)
+                print("UPDATING: ", slider_previous, slider_current, diff)
+                slider_previous = slider_current
 
-        slider_current = sum(slider_values)/NUM_SMOOTHED_VALUES
-        diff = math.trunc(slider_value - slider_current)
+        ble.start_advertising(advertisement)
 
-        # print('y:{0}, V:{1}; raw:{2}'.format(slider_current, voltage, pot_in.value))
-        if abs(diff) >= CHANGE_TOLERANCE:
-            print(slider_current, voltage, diff)
-            mouse.move(y=diff)
-            slider_value = slider_current
 
-    ble.start_advertising(advertisement)
+if __name__ == "__main__":
+    main_event_loop()
